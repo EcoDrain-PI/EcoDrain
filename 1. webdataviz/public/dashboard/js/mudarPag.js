735d3a988abfe2a4f43f42912a22fe3239242e1e
ThiagoFukunaga <thiago.fukunaga@sptech.school>
2025-05-17 13:49:38 -0300

    Ajustes no login e index

---
+++ 1. webdataviz/public/dashboard/js/mudarPag.js
@@ -0,0 +1,25 @@
+function mudarPagMain(){
+    window.location.assign("../public/dashboard/mainPage.html")
+}
+
+function mudarPagParametros(){
+    window.location.assign("../public/dashboard/parametro.html")
+}
+
+
+// zonas
+function mudarPagZonaNorte(){
+    window.location.assign("../public/dashboard/zonaNorte.html")
+}
+
+function mudarPagZonaLeste(){
+    window.location.assign("../public/dashboard/zonaLeste.html")
+}
+
+function mudarPagZonaOeste(){
+    window.location.assign("../public/dashboard/zonaOeste.html")
+}
+
+function mudarPagZonaSul(){
+    window.location.assign("../public/dashboard/zonaSul.html")
+}
