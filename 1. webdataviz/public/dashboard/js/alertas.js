9ff78811755e59add85445fc19c32a0d3fb1aee1
Gabriel1Ribeiro <gabriel.hribeiro@sptech.school>
2025-05-19 02:44:12 -0300

    Alterações na parte de 'avisos'

---
+++ 1. webdataviz/public/dashboard/js/alertas.js
@@ -0,0 +1,413 @@
+const select = document.getElementById('filtroSelect');
+
+
+  const alertaRiscoX = document.getElementById('div_ultima_hora');
+  const alertaRiscoY = document.getElementById('div_ultimo_dia');
+  const alertaRiscoZ = document.getElementById('div_ultima_semana');
+  select.addEventListener('change', function () {
+    // Oculta todas as divs
+
+    // let alertaRiscoX = div_ultima_hora.value;
+    // let alertaRiscoY = div_ultimo_dia.value;
+    // let alertaRiscoZ = div_ultima_semana.value;
+
+
+
+    alertaRiscoX.style.display = 'none';
+    alertaRiscoY.style.display = 'none';
+    alertaRiscoZ.style.display = 'none';
+
+
+    // Mostra a div correspondente
+    if (this.value === 'ultimaHora') {
+      alertaRiscoX.style.display = 'block';
+    } else if (this.value === 'ultimas24') {
+      alertaRiscoX.style.display = 'block';
+      alertaRiscoY.style.display = 'block';
+    } else if (this.value === 'ultimaSemana') {
+      alertaRiscoX.style.display = 'block';
+      alertaRiscoY.style.display = 'block';
+      alertaRiscoZ.style.display = 'block';
+    }
+  }
+);
+
+// ALERTAS!!!
+
+//ALERTA "NORMAL"
+
+// const canvasNormalX = document.getElementById('metrica-Barras-alerta-normal-x').getContext('2d');
+
+// const dadosNormalX = {
+//   2025: [
+//     [200, 10, 20, 2],
+//     [180, 12, 24, 0],
+//     [160, 15, 27, 31]
+//   ]
+// };
+
+// // Atualizado por semanas
+// const labelsNormalX = ['00:15:00', '00:30:00', '00:45:00', '01:00:00'];
+
+// // Criação inicial do gráfico
+// const graficoNormalX = new Chart(canvasNormalX, {
+//   type: 'bar',
+//   data: {
+//     labels: labelsNormalX,
+//     datasets: [
+//       {
+//         label: 'Rua x1',
+//         data: dadosNormalX[2025][0],
+//         borderColor: 'black',
+//         backgroundColor: 'rgb(90, 245, 227)'
+//       },
+//       {
+//         label: 'Rua x2',
+//         data: dadosNormalX[2025][1],
+//         backgroundColor: 'rgb(30, 208, 186)'
+//       },
+//       {
+//         label: 'Rua x3',
+//         data: dadosNormalX[2025][2],
+//         backgroundColor: 'rgb(42, 157, 143)'
+//       },
+
+//     ]
+//   },
+//   options: {
+//     responsive: true,
+//     scales: {
+//       y: {
+//         beginAtZero: true,
+//         ticks: { color: '#ffffff' },
+//         grid: { color: 'rgba(255, 255, 255, 0.2)' }
+//       },
+//       x: {
+//         ticks: { color: '#ffffff' },
+//         grid: { color: 'rgba(255, 255, 255, 0.2)' }
+//       }
+//     },
+//     plugins: {
+//       legend: {
+//         labels: { color: '#ffffff' }
+//       }
+//     }
+//   }
+// });
+
+// // Alerta de Normal Y
+
+// const canvasNormalY = document.getElementById('metrica-Barras-alerta-normal-y').getContext('2d');
+
+// const dadosNormalY = {
+//   2025: [
+//     [10, 150, 20, 1],
+//     [20, 20, 20, 2],
+//     [30, 20, 20, 3]
+//   ]
+// };
+
+// // Atualizado por semanas
+// const labelsNormalY = ['06:00', '12:00', '18:00', '23:00'];
+
+// // Criação inicial do gráfico
+// const graficoNormalY = new Chart(canvasNormalY, {
+//   type: 'bar',
+//   data: {
+//     labels: labelsNormalY,
+//     datasets: [
+//       {
+//         label: 'Rua y1',
+//         data: dadosNormalY[2025][0],
+//         borderColor: 'black',
+//         backgroundColor: 'rgb(90, 245, 227)'
+//       },
+//       {
+//         label: 'Rua y2',
+//         data: dadosNormalY[2025][1],
+//         backgroundColor: 'rgb(30, 208, 186)'
+//       },
+//       {
+//         label: 'Rua y3',
+//         data: dadosNormalY[2025][2],
+//         backgroundColor: 'rgb(42, 157, 143)'
+//       },
+
+//     ]
+//   },
+//   options: {
+//     responsive: true,
+//     scales: {
+//       y: {
+//         beginAtZero: true,
+//         ticks: { color: '#ffffff' },
+//         grid: { color: 'rgba(255, 255, 255, 0.2)' }
+//       },
+//       x: {
+//         ticks: { color: '#ffffff' },
+//         grid: { color: 'rgba(255, 255, 255, 0.2)' }
+//       }
+//     },
+//     plugins: {
+//       legend: {
+//         labels: { color: '#ffffff' }
+//       }
+//     }
+//   }
+// });
+
+
+// Alerta de Normal z
+
+
+// Alerta de Normal Y
+
+// const canvasNormalZ = document.getElementById('metrica-Barras-alerta-normal-z').getContext('2d');
+
+// const dadosNormalZ = {
+//   2025: [
+//     [10, 150, 20, 1, 10, 150, 20],
+//     [20, 15, 5, 2, 33, 42, 31,],
+//     [30, 20, 150, 3, 10, 42, 20,]
+//   ]
+// };
+
+// // Atualizado por semanas
+// const labelsNormalZ = [ 'Dom', 'Seg', 'Ter', 'Quar', 'Quin', 'Sex', 'Sáb'];
+
+// // Criação inicial do gráfico
+// const graficoNormalZ = new Chart(canvasNormalZ, {
+//   type: 'bar',
+//   data: {
+//     labels: labelsNormalZ,
+//     datasets: [
+//       {
+//         label: 'Rua z1',
+//         data: dadosNormalZ[2025][0],
+//         borderColor: 'black',
+//         backgroundColor: 'rgb(90, 245, 227)'
+//       },
+//       {
+//         label: 'Rua z2',
+//         data: dadosNormalZ[2025][1],
+//         backgroundColor: 'rgb(30, 208, 186)'
+//       },
+//       {
+//         label: 'Rua z3',
+//         data: dadosNormalZ[2025][2],
+//         backgroundColor: 'rgb(42, 157, 143)'
+//       },
+
+//     ]
+//   },
+//   options: {
+//     responsive: true,
+//     scales: {
+//       y: {
+//         beginAtZero: true,
+//         ticks: { color: '#ffffff' },
+//         grid: { color: 'rgba(255, 255, 255, 0.2)' }
+//       },
+//       x: {
+//         ticks: { color: '#ffffff' },
+//         grid: { color: 'rgba(255, 255, 255, 0.2)' }
+//       }
+//     },
+//     plugins: {
+//       legend: {
+//         labels: { color: '#ffffff' }
+//       }
+//     }
+//   }
+// });
+
+
+
+// ALERTAS DE "RISCO"
+
+// Alerta RISCO X
+
+const canvasRiscoX = document.getElementById('metrica-Barras-alerta-risco-x').getContext('2d');
+
+const dadosRiscoX = {
+  2025: [
+    [200, 10, 20, 2],
+    [180, 12, 24, 0],
+    [160, 15, 27, 31]
+  ]
+};
+
+// Atualizado por semanas
+const labelsRiscoX = ['00:15:00', '00:30:00', '00:45:00', '01:00:00'];
+
+// Criação inicial do gráfico
+const graficoRiscoX = new Chart(canvasRiscoX, {
+  type: 'bar',
+  data: {
+    labels: labelsRiscoX,
+    datasets: [
+      {
+        label: 'Rua x1',
+        data: dadosRiscoX[2025][0],
+        borderColor: 'black',
+        backgroundColor: 'rgb(90, 245, 227)'
+      },
+      {
+        label: 'Rua x2',
+        data: dadosRiscoX[2025][1],
+        backgroundColor: 'rgb(30, 208, 186)'
+      },
+      {
+        label: 'Rua x3',
+        data: dadosRiscoX[2025][2],
+        backgroundColor: 'rgb(42, 157, 143)'
+      },
+
+    ]
+  },
+  options: {
+    responsive: true,
+    scales: {
+      y: {
+        beginAtZero: true,
+        ticks: { color: '#ffffff' },
+        grid: { color: 'rgba(255, 255, 255, 0.2)' }
+      },
+      x: {
+        ticks: { color: '#ffffff' },
+        grid: { color: 'rgba(255, 255, 255, 0.2)' }
+      }
+    },
+    plugins: {
+      legend: {
+        labels: { color: '#ffffff' }
+      }
+    }
+  }
+});
+
+// Alerta de risco Y
+
+const canvasRiscoY = document.getElementById('metrica-Barras-alerta-risco-y').getContext('2d');
+
+const dadosRiscoY = {
+  2025: [
+    [10, 150, 20, 1],
+    [20, 20, 20, 2],
+    [30, 20, 20, 3]
+  ]
+};
+
+// Atualizado por semanas
+const labelsRiscoY = ['06:00', '12:00', '18:00', '23:00'];
+
+// Criação inicial do gráfico
+const graficoRiscoY = new Chart(canvasRiscoY, {
+  type: 'bar',
+  data: {
+    labels: labelsRiscoY,
+    datasets: [
+      {
+        label: 'Rua y1',
+        data: dadosRiscoY[2025][0],
+        borderColor: 'black',
+        backgroundColor: 'rgb(90, 245, 227)'
+      },
+      {
+        label: 'Rua y2',
+        data: dadosRiscoY[2025][1],
+        backgroundColor: 'rgb(30, 208, 186)'
+      },
+      {
+        label: 'Rua y3',
+        data: dadosRiscoY[2025][2],
+        backgroundColor: 'rgb(42, 157, 143)'
+      },
+
+    ]
+  },
+  options: {
+    responsive: true,
+    scales: {
+      y: {
+        beginAtZero: true,
+        ticks: { color: '#ffffff' },
+        grid: { color: 'rgba(255, 255, 255, 0.2)' }
+      },
+      x: {
+        ticks: { color: '#ffffff' },
+        grid: { color: 'rgba(255, 255, 255, 0.2)' }
+      }
+    },
+    plugins: {
+      legend: {
+        labels: { color: '#ffffff' }
+      }
+    }
+  }
+});
+
+
+// Alerta de risco z
+
+
+// Alerta de risco Y
+
+const canvasRiscoZ = document.getElementById('metrica-Barras-alerta-risco-Z').getContext('2d');
+
+const dadosRiscoZ = {
+  2025: [
+    [10, 150, 20, 1, 10, 150, 20],
+    [20, 15, 5, 2, 33, 42, 31,],
+    [30, 20, 150, 3, 10, 42, 20,]
+  ]
+};
+
+// Atualizado por semanas
+const labelsRiscoZ = [ 'Dom', 'Seg', 'Ter', 'Quar', 'Quin', 'Sex', 'Sáb'];
+
+// Criação inicial do gráfico
+const graficoRiscoZ = new Chart(canvasRiscoZ, {
+  type: 'bar',
+  data: {
+    labels: labelsRiscoZ,
+    datasets: [
+      {
+        label: 'Rua z1',
+        data: dadosRiscoZ[2025][0],
+        borderColor: 'black',
+        backgroundColor: 'rgb(90, 245, 227)'
+      },
+      {
+        label: 'Rua z2',
+        data: dadosRiscoZ[2025][1],
+        backgroundColor: 'rgb(30, 208, 186)'
+      },
+      {
+        label: 'Rua z3',
+        data: dadosRiscoZ[2025][2],
+        backgroundColor: 'rgb(42, 157, 143)'
+      },
+
+    ]
+  },
+  options: {
+    responsive: true,
+    scales: {
+      y: {
+        beginAtZero: true,
+        ticks: { color: '#ffffff' },
+        grid: { color: 'rgba(255, 255, 255, 0.2)' }
+      },
+      x: {
+        ticks: { color: '#ffffff' },
+        grid: { color: 'rgba(255, 255, 255, 0.2)' }
+      }
+    },
+    plugins: {
+      legend: {
+        labels: { color: '#ffffff' }
+      }
+    }
+  }
+});
--- 1. webdataviz/src/models/medidaModel.js
+++ 1. webdataviz/src/models/medidaModel.js
@@ -1,28 +1,27 @@
 var database = require("../database/config");
 
-function buscarUltimasMedidas(idAquario, limite_linhas) {
+function buscarUltimasMedidas(idMonitoramento, limite_linhas) {
 
     var instrucaoSql = `SELECT 
-        dht11_temperatura as temperatura, 
-        dht11_umidade as umidade,
-                        momento,
-                        DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico
-                    FROM medida
-                    WHERE fk_aquario = ${idAquario}
+        altura_lixo as altura_do_Lixo,
+            data_monitoramento,
+                        DATE_FORMAT(data_monitoramento,'%H:%i:%s') as dt_monitoramento
+                    FROM lotacao
+                    WHERE fkSensor = ${idMonitoramento}
                     ORDER BY id DESC LIMIT ${limite_linhas}`;
 
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
 
-function buscarMedidasEmTempoReal(idAquario) {
+function buscarMedidasEmTempoReal(idMonitoramento) {
 
     var instrucaoSql = `SELECT 
-        dht11_temperatura as temperatura, 
-        dht11_umidade as umidade,
-                        DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico, 
-                        fk_aquario 
-                        FROM medida WHERE fk_aquario = ${idAquario} 
+        altura_lixo as altura_do_Lixo,
+            data_monitoramento,
+                        DATE_FORMAT(data_monitoramento,'%H:%i:%s') as dt_monitoramento
+                    FROM lotacao
+                    WHERE fkSensor = ${idMonitoramento}
                     ORDER BY id DESC LIMIT 1`;
 
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
