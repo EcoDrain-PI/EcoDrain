3388a3b220fdd6738f3e4c3822e86107507725a6
AdamanLino <adaman.santos@sptech.school>
2025-05-23 17:42:17 -0300

    grafico de nivel de lotacao do bueiro funcionando com o bd

--- 1. webdataviz/src/models/medidaModel.js
+++ 1. webdataviz/src/models/medidaModel.js
@@ -1,34 +1,19 @@
 var database = require("../database/config");
 
-function buscarUltimasMedidas(idMonitoramento, limite_linhas) {
-
-    var instrucaoSql = `SELECT 
-        altura_lixo as altura_do_Lixo,
-            data_monitoramento,
-                        DATE_FORMAT(data_monitoramento,'%H:%i:%s') as dt_monitoramento
-                    FROM lotacao
-                    WHERE fkSensor = ${idMonitoramento}
-                    ORDER BY id DESC LIMIT ${limite_linhas}`;
-
-    console.log("Executando a instrução SQL: \n" + instrucaoSql);
-    return database.executar(instrucaoSql);
-}
-
-function buscarMedidasEmTempoReal(idMonitoramento) {
-
-    var instrucaoSql = `SELECT 
-        altura_lixo as altura_do_Lixo,
-            data_monitoramento,
-                        DATE_FORMAT(data_monitoramento,'%H:%i:%s') as dt_monitoramento
-                    FROM lotacao
-                    WHERE fkSensor = ${idMonitoramento}
-                    ORDER BY id DESC LIMIT 1`;
+function buscarUltimasMedidas(idBueiro, limite_linhas) {
+    var instrucaoSql = `
+    SELECT b.idBueiro,
+    l.altura_lixo,
+    l.data_monitoramento
+    FROM bueiro b
+    JOIN sensor s ON s.fkBueiro = b.idBueiro
+    JOIN lotacao l ON l.fkSensor = s.idSensor
+    ORDER BY b.idBueiro, l.data_monitoramento DESC`;
 
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
 
 module.exports = {
-    buscarUltimasMedidas,
-    buscarMedidasEmTempoReal
-}
+    buscarUltimasMedidas
+};
