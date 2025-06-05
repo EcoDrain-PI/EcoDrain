912cec89348423885056de7470cfa976110144e4
PedrooAmaral <pedro.amaral@sptech.school>
2025-06-05 10:38:57 -0300

    adição da rota para o card-atenção

--- 1. webdataviz/src/routes/medidas.js
+++ 1. webdataviz/src/routes/medidas.js
@@ -11,4 +11,17 @@ router.get("/tempo-real/:idBueiro", function (req, res) {
     medidaController.buscarMedidasEmTempoReal(req, res);
 })
 
+router.get("/atencao", async function (req, res) {
+  try {
+    const [linhas] = await db.execute(`
+      SELECT COUNT(*) AS atencao
+      FROM lotacao
+      WHERE altura_lixo <= 139 AND altura_lixo > 50
+    `);
+    res.json(linhas[0]);
+  } catch (erro) {
+    console.error("ERRO AO CONTAR ALERTAS:", erro);
+    res.status(500).json({ erro: "Erro ao contar alertas" });
+  }
+});
 module.exports = router;
