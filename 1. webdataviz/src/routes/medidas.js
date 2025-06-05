797ca53288fa11c12c2b4b9ebdf1350fcd774cf3
PedrooAmaral <pedro.amaral@sptech.school>
2025-06-05 10:52:05 -0300

    adição da rota de risco

--- 1. webdataviz/src/routes/medidas.js
+++ 1. webdataviz/src/routes/medidas.js
@@ -24,4 +24,18 @@ router.get("/atencao", async function (req, res) {
     res.status(500).json({ erro: "Erro ao contar alertas" });
   }
 });
+router.get("/risco", async function (req, res) {
+  try {
+    const [linhas] = await db.execute(`
+      SELECT COUNT(*) AS risco
+      FROM lotacao
+      WHERE altura_lixo <= 50
+    `);
+    res.json(linhas[0]);
+  } catch (erro) {
+    console.error("ERRO AO CONTAR ALERTAS DE RISCO:", erro);
+    res.status(500).json({ erro: "Erro ao contar alertas de risco" });
+  }
+});
+
 module.exports = router;
